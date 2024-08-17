a8d8f5d3f1344e019b8a5ca8755029b8070875d5
MehekZahid <mehekzahid365@gmail.com>
2024-08-17 23:02:42 +0500

    logic program

--- flutter_application_1/lib/task13.dart
+++ flutter_application_1/lib/task13.dart
@@ -2,13 +2,21 @@
 
 import 'package:flutter/material.dart';
 
-class Task13 extends StatelessWidget {
+class Task13 extends StatefulWidget {
   const Task13({super.key});
 
   @override
+  State<Task13> createState() => _Task13State();
+}
+
+class _Task13State extends State<Task13> {
+  bool isvideocall = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 238, 234, 234),
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 238, 234, 234),
         leading: Icon(
           Icons.arrow_back,
           size: 30,
@@ -23,46 +31,111 @@ class Task13 extends StatelessWidget {
         ),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(
-                'Video Call',
-                style: TextStyle(
-                  fontSize: 15,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isvideocall = true;
+                  });
+                },
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Text(
+                      'Video Call',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: isvideocall ? Colors.amber : Colors.grey,
+                      ),
+                    ),
+                    Container(
+                      height: 6,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: isvideocall ? Colors.amber : Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                'Audio Call',
-                style: TextStyle(
-                  fontSize: 15,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isvideocall = false;
+                  });
+                },
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Text(
+                      'Audio Call',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: isvideocall ? Colors.grey : Colors.amber,
+                      ),
+                    ),
+                    Container(
+                      height: 6,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(40),
+                        color: isvideocall ? Colors.grey : Colors.amber,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
+          SizedBox(
+            height: 20,
+          ),
           Container(
-            height: 80,
-            width: 360,
+            height: 120,
+            width: 330,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
+              borderRadius: BorderRadius.circular(20),
               color: Colors.white,
             ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Column(children: [
-                  Image.asset('assets/images/lesson.jpg'),
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset(
+                    'assets/images/pic.jpg',
+                    width: 100,
+                  ),
                 ]),
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       'Stephen N',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
-                    Text("Video Call      Scheduled"),
+                    Text(
+                      "Video Call      Scheduled",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
                     Text('11:30 AM'),
                   ],
                 )
@@ -70,85 +143,151 @@ class Task13 extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 10,
+            height: 20,
           ),
-          Center(
-            child: Container(
-                height: 100,
-                width: 600,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey, width: 2.0),
-                    borderRadius: BorderRadius.circular(30.0)),
-                child: Row(
+          Container(
+              height: 70,
+              width: 330,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color.fromARGB(255, 248, 201, 60),
+                        ),
+                        child: Icon(
+                          Icons.notifications_active_outlined,
+                          size: 25,
+                          color: Color.fromARGB(255, 197, 158, 39),
+                        ),
+                      ),
+                      const Text('Follow'),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color.fromARGB(255, 248, 201, 60),
+                        ),
+                        child: Icon(
+                          Icons.message_outlined,
+                          size: 25,
+                          color: Color.fromARGB(255, 197, 158, 39),
+                        ),
+                      ),
+                      const Text('Message'),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color.fromARGB(255, 248, 201, 60),
+                        ),
+                        child: Icon(
+                          Icons.favorite_border_outlined,
+                          size: 25,
+                          color: Color.fromARGB(255, 197, 158, 39),
+                        ),
+                      ),
+                      const Text('Favorite'),
+                    ],
+                  ),
+                ],
+              )),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                '     I made this design to tell you that i am learning\n     flutter from last 3 months and i am so happy with \n     my progress in UI designs',
+                style: TextStyle(fontSize: 17),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Text(
+                "   Class Details",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 18.0),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color.fromARGB(255, 248, 201, 60),
-                          ),
-                          child: const SizedBox(
-                            width: 60.0,
-                            height: 40.0,
-                            child: Icon(
-                              Icons.notifications,
-                              size: 30,
-                              color: Color.fromARGB(255, 197, 158, 39),
-                            ),
-                          ),
-                        ),
-                        const Text('Follow'),
-                      ],
+                    Text(
+                      "Monday : 24 March 2024",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color.fromARGB(255, 248, 201, 60),
-                          ),
-                          child: const SizedBox(
-                            width: 60.0,
-                            height: 40.0,
-                            child: Icon(
-                              Icons.message,
-                              size: 30,
-                              color: Color.fromARGB(255, 197, 158, 39),
-                            ),
-                          ),
-                        ),
-                        const Text('Message'),
-                      ],
+                    SizedBox(
+                      height: 15,
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color.fromARGB(255, 248, 201, 60),
-                          ),
-                          child: const SizedBox(
-                            width: 60.0,
-                            height: 40.0,
-                            child: Icon(
-                              Icons.favorite,
-                              size: 30,
-                              color: Color.fromARGB(255, 197, 158, 39),
-                            ),
-                          ),
-                        ),
-                        const Text('Favorite'),
-                      ],
+                    Text(
+                      "Total Time : 30 Mins",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "Teusday : 27 March 2024",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "Total Time : 30 Mins",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                   ],
-                )),
-          ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
